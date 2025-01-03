23d71b9d23cebb2b58194a8438a4fe6bde26bd90
Metalted <metalted@gmail.com>
2025-01-03 18:34:08 +0100

    Updated version number

--- Plugin.cs
+++ Plugin.cs
@@ -11,7 +11,7 @@ namespace BPX
     {
         public const string pluginGuid = "com.metalted.zeepkist.blueprintsX";
         public const string pluginName = "Blueprints X";
-        public const string pluginVersion = "2.2";
+        public const string pluginVersion = "2.3";
 
         public static Plugin Instance;
         public string pluginPath;
